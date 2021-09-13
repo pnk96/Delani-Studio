be355fe43da4fba0179e91250b16d26bbb61d1b8
Peter Karuku <peter.karuku@student.moringaschool.com>
2021-09-13 21:29:47 +0300

    style the contact form

---
+++ js/script.js
@@ -0,0 +1,49 @@
+/*first paragraph - DESIGN*/
+
+$(document).ready(function() {
+  $("#h-design").click(function() {
+    $("#services1").show();
+    $("#h-design").hide();
+  })
+})
+
+$(document).ready(function() {
+$("#services1").click(function() {
+  $("#h-design").show();
+  $("#services1").hide();
+  })
+})
+
+
+/*second paragraph - DEVElOPMENT*/
+
+$(document).ready(function() {
+  $("#h-development").click(function() {
+    $("#services2").show();
+    $("#h-development").hide();
+  })
+})
+
+$(document).ready(function() {
+$("#services2").click(function() {
+  $("#h-development").show();
+  $("#services2").hide();
+  })
+})
+
+
+/*third paragraph - PRODUCT MANAGEMENT*/
+
+$(document).ready(function() {
+  $("#h-pmanagement").click(function() {
+    $("#services3").show();
+    $("#h-pmanagement").hide();
+  })
+})
+
+$(document).ready(function() {
+$("#services3").click(function() {
+  $("#h-pmanagement").show();
+  $("#services3").hide();
+  })
+})
